5730997283aad05a77c91a2cabc91abd81ece2ca
yang <knisfp97@gmail.com>
2025-01-30 23:14:57 +0900

    ダミーデータ用シーダファイルと画像の追加、bladeの編集

---
+++ src/database/seeders/CategoriesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         DB::table('categories')->insert([
+            ['category' => '腕時計', 'item_id' => 1],
+            ['category' => '家電', 'item_id' => 2],
+            ['category' => '食品', 'item_id' => 3],['category' => '靴', 'item_id' => 4],
+            ['category' => '家電', 'item_id' => 5],
+            ['category' => '家電', 'item_id' => 6],
+            ['category' => '鞄', 'item_id' => 7],
+            ['category' => 'キッチン用品', 'item_id' => 8],
+            ['category' => 'キッチン用品', 'item_id' => 9],
+            ['category' => 'コスメ', 'item_id' => 10,],
+         ]);
+    }
+}
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         UsersTableSeeder::class,
         ItemsTableSeeder::class,
         ConditionsTableSeeder::class,
+        CategoriesTableSeeder ::class,
     ]);
     }
 }
